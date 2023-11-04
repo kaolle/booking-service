a6f89e5b0113e8a21d4e3842e6550ac34331e960
stefan.karlsson <stefan.karlsson@assaabloy.com>
2023-11-04 13:24:00 +0100

    fixed formatting

--- src/main/java/pb/se/bookingservice/port/rest/exception/GlobalExceptionAdvisor.java
+++ src/main/java/pb/se/bookingservice/port/rest/exception/GlobalExceptionAdvisor.java
@@ -9,17 +9,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import pb.se.bookingservice.application.MemberNotFoundException;
 import pb.se.bookingservice.application.TimePeriodException;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 @ControllerAdvice
 public class GlobalExceptionAdvisor extends ResponseEntityExceptionHandler {
-
 
     @ExceptionHandler(TimePeriodException.class)
     public ResponseEntity<Object> toResponse(TimePeriodException e, WebRequest request) {
         return new ResponseEntity<>(e.getExistingBooking(), HttpStatus.CONFLICT);
     }
+
     @ExceptionHandler(MemberNotFoundException.class)
     public ResponseEntity<Object> toResponse(MemberNotFoundException e, WebRequest request) {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
