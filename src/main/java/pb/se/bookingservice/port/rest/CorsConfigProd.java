29d10116b252d23e9ee90b0ae75c67501ff6eb38
kaolle <kaolle@gmail.com>
2026-09-08 16:00:28 +0200

    Added CORS for https://palma-booking.web.app

--- src/main/java/pb/se/bookingservice/port/rest/CorsConfigProd.java
+++ src/main/java/pb/se/bookingservice/port/rest/CorsConfigProd.java
@@ -14,7 +14,8 @@ public class CorsConfigProd {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("https://palma-boknings-kalender-d2116475a2d8.herokuapp.com/"); // You can specify specific origins here, e.g., "https://your-frontend-domain.com" TODO
+        config.addAllowedOrigin("https://palma-boknings-kalender-d2116475a2d8.herokuapp.com");
+        config.addAllowedOrigin("https://palma-booking.web.app");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
 
